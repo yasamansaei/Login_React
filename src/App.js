093dfbe8a9f980dba5757d2815dddecb94452af6
yasaman <yasaman@DESKTOP-K1F0GQI>
2021-04-25 20:12:23 +0430

    first commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,47 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.css'
-import ToDoList from "./Components/ToDoList";
-import {BrowserRouter, Route} from 'react-router-dom'
-import About from "./Components/About";
-import Header from "./Components/Header";
+import Login from './Components/Login'
+import SignUp from './Components/SignUp'
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import Welcome from "./Components/Welcome";
 
 function App() {
   return (
-    <React.Fragment>  <BrowserRouter>
-    <Header/>
-    <div className="container">
-      
-      <div className="main">
-      
-        <Route path="/" exact >
-        <ToDoList/>
-        </Route>
-        <Route path={`/about/id=:id&Nme=:Name`} >
-          <About/>
-        </Route>
-        <Route path="/ConatactUs/:id" component={()=><h2>ConatactUs</h2>}></Route>
-        <Route path="/News" component={()=><h2>News</h2>}></Route>
-        
+    <React.Fragment>  
+      <Router>
+          <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/sign-in"}>RemoteStack</Link>
+          <Link className="navbar-brand" to={"/welcome"}>welcome</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>Sign in</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="outer">
+        <div className="inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/sign-in" component={Login} />
+            <Route path="/sign-up" component={SignUp} />
+            <Route path="/welcome" component={Welcome} />
+          </Switch>
+        </div>
       </div>
-    </div> </BrowserRouter></React.Fragment>
+    </div>
+    </Router>
+
+    </React.Fragment>
   );
 }
 
